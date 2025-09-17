ecc382838d0a06e66f099b0202e71c333fd697a8
lianaminasyan08-beep <lianaminasyan08@gmail.com>
2025-09-17 22:55:17 +0400

    Update views.py

--- resume_analyzer/views.py
+++ resume_analyzer/views.py
@@ -1,4 +1,39 @@
 from django.http import JsonResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import logout
+
+
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+# Create your views here.
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+from django.contrib.auth import authenticate, login
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')
+        else:
+            return render(request, 'login.html', {'error': 'Неверные данные'})
+    return render(request, 'login.html')
+
 
 def analyze_resume(request):
     sample_cv = {
@@ -11,3 +46,27 @@ def analyze_resume(request):
         "missing_skills": ["REST APIs", "SQL"]
     }
     return JsonResponse({"cv": sample_cv, "analysis": result})
+
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+# password change
+@login_required
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            return redirect('dashboard')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'change_password.html', {'form': form})
+
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
