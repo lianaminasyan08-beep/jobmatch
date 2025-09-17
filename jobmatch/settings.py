c57e89442ed6abd10fc5542ce944cde64c5d9508
lianaminasyan08-beep <lianaminasyan08@gmail.com>
2025-09-17 22:52:30 +0400

    Update settings.py

--- jobmatch/settings.py
+++ jobmatch/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'resume_analyzer',
-    'front',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +127,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
