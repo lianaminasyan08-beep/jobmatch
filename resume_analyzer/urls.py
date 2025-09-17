31169d82f879cd2b54cf5bae3c36eba8a414e018
lianaminasyan08-beep <lianaminasyan08@gmail.com>
2025-09-17 22:54:34 +0400

    Update urls.py

--- resume_analyzer/urls.py
+++ resume_analyzer/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('change-password/', views.change_password, name='change_password'),
     path('analyze/', views.analyze_resume, name='analyze_resume'),
 ]
