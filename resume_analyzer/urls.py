f9d5b5ca0784205df0fb8cfffbd2e815119ca277
lianaminasyan08-beep <lianaminasyan08@gmail.com>
2025-09-19 00:11:19 +0400

    Update urls.py

--- resume_analyzer/urls.py
+++ resume_analyzer/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
     path('change-password/', views.change_password, name='change_password'),
     path('analyze/', views.analyzer_ui, name='analyze_ui'),
+    path('portfolio/', views.portfolio_view, name='portfolio'),
 ]
